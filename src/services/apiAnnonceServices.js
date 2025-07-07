201de29bc7c301d9e488237608236ead958c31b6
Amen Allah Jouila <101395490+amenallah0@users.noreply.github.com>
2025-07-07 11:58:38 +0100

    Update apiAnnonceServices.js

--- src/services/apiAnnonceServices.js
+++ src/services/apiAnnonceServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/annonces'; // Remplacez par votre URL d'API
+const API_URL = process.env.REACT_APP_API_URL + '/api/annonces'; // Remplacez par votre URL d'API
 
 const ApiAnnonceService = {
     getAllAnnonces: async () => {
