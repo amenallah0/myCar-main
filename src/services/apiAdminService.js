01d11d2a0dba746163dfdcbc9df8bdcd82387f63
Amen Allah Jouila <101395490+amenallah0@users.noreply.github.com>
2025-07-07 11:58:50 +0100

    Update apiAdminService.js

--- src/services/apiAdminService.js
+++ src/services/apiAdminService.js
@@ -1,6 +1,6 @@
 import { axiosInstance } from './apiUserServices';
 
-const API_URL = 'http://localhost:8081/api';
+const API_URL = process.env.REACT_APP_API_URL + '/api';
 
 const apiAdminService = {
   // User Management
